f15f384368a2b9ef60217c68ab488192e9418f33
Rakku <dudfkr7845@gmail.com>
2022-08-19 00:09:08 +0900

    Initialize project

---
+++ src/main/java/com/rakku/covidinline/CovidInLineApplication.java
@@ -0,0 +1,13 @@
+package com.rakku.covidinline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidInLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidInLineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rakku/covidinline/CovidInLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakku.covidinline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidInLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
